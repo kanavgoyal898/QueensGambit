9f84394b2de0a899d8db3ab5b2c87467d0bb87a0
Kanav Goyal <kanavgoyal898@gmail.com>
2024-07-08 13:40:21 +0530

    Add SVG support

--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 ## Overview
 QueenGambit is a Python-based chess engine implemented using the `python-chess` library. It allows users to play against it at different difficulty levels or watch it play against itself. Minimax, depth-pruning, and alpha-beta pruning are crucial techniques in computer chess programming, especially given the vast number of possible board positions starting from the beginning of the game.
 
+<div style="text-align: center;">
+  <img src="./chess.png" alt="Preview" style="width: 64%;">
+</div>
+
+---
+
 1. **Minimax Algorithm**: Minimax is fundamental for decision-making in game theory and chess AI. It works by recursively evaluating possible moves to determine the optimal move for the current player assuming the opponent plays optimally. This is essential because without such a method, evaluating all possible moves to the end of the game (especially considering the branching factor of chess) would be computationally infeasible.
 
 2. **Depth-Pruning**: Depth-pruning limits the depth to which the minimax algorithm explores possible moves. This is critical in chess because it helps balance between depth of search (finding better moves) and computational feasibility. Without depth-pruning, the search space would grow exponentially, making it impractical to compute.
@@ -10,11 +16,6 @@ QueenGambit is a Python-based chess engine implemented using the `python-chess` 
 3. **Alpha-Beta Pruning**: Alpha-beta pruning further enhances efficiency by pruning branches of the search tree that cannot possibly influence the final decision. It reduces the number of nodes evaluated by the minimax algorithm by taking advantage of bounds (alpha and beta values) to discard irrelevant subtrees. This technique significantly speeds up the search process in chess AI.
 
 Given the vast number of possible chess board positions (estimated around **1e43** to **1e50**), these techniques are indispensable. They allow chess engines to make intelligent decisions within a reasonable time frame by narrowing down the search space and focusing on the most promising moves. Without them, achieving competitive playing strength would be nearly impossible due to the sheer computational complexity involved.
-
-
-<div style="text-align: center;">
-  <img src="./chess.png" alt="Preview" style="width: 64%;">
-</div>
 
 ## Library Installations
 Before using QueenGambit, make sure you have the following libraries installed:
@@ -66,6 +67,7 @@ To play the game:
 4. The engine will respond with its move, and the game continues until completion.
 
 ## Additional Notes
+
 - **Undo Move:** Typing "UNDO" allows the user to undo their last move.
 - **End Game:** Typing "END" terminates the game.
 - **Outcome:** After the game ends, the result (win/lose/draw) is displayed.
--- models/main.py
+++ models/main.py
@@ -1,44 +1,57 @@
 import cprint
+import chess.svg
 import chess as ch
 import ChessEngine as engine
 
+def save_board_state(board, filename='chess_board.svg'):
+    """Save the current state of the board to an SVG file."""
+    image = chess.svg.board(board)
+    with open(filename, 'w') as f:
+        f.write(image)
+    f.close()
+
 def play_engine_move(board, color, max_depth):
+    """Make a move using the chess engine and update the board state."""
     chessbot = engine.Engine(board, color, max_depth)
     move = chessbot.get_best_move()
     board.push(move)
-    print('===============')
-    print(board)
-    print('===============')
+    save_board_state(board)
 
 def play_human_move(board):
+    """Prompt the human player to make a move and update the board state."""
     try:
         if len(list(board.legal_moves)) == 0:
             return
         else:
-            cprint.cprint.err(f'legal moves: {board.legal_moves}', interrupt=False)
+            legal_moves = list(board.legal_moves)
+            legal_moves = [move.uci() for move in legal_moves]
+            legal_moves_str = ' '.join(legal_moves)
+            cprint.cprint.err(f'legal moves: {legal_moves_str}', interrupt=False)
             move = input('enter your move: ')
             if move.upper() == 'UNDO':
                 try:
                     board.pop()
                     board.pop()
+                    save_board_state(board)
                     play_human_move(board)
                 except IndexError:
                     cprint.cprint.fatal("no more moves to undo...")
-                    play_human_move(board)
             elif move.upper() == 'END':
                 board.reset()
+                save_board_state(board)
                 cprint.cprint.fatal('the game is now terminated...')
                 exit()
             else:
                 board.push_san(move)
-                print('===============')
-                print(board)
-                print('===============')
+                save_board_state(board)
     except ValueError:
         cprint.cprint.fatal('invalid move! please try again...')
         play_human_move(board)
 
 def start_game(board, color, max_depth):
+    """Start the game and alternate moves between the human player and the engine."""
+    cprint.cprint.info('enter UNDO/END to interrupt the game')
+
     if color in ['b', 'black']:
         while not board.is_game_over():
             cprint.cprint.ok('the engine is thinking...')
@@ -50,22 +63,27 @@ def start_game(board, color, max_depth):
             cprint.cprint.ok('the engine is thinking...')
             play_engine_move(board, ch.BLACK, max_depth)
 
-    if board.outcome().winner == None:
+    save_board_state(board)
+
+    outcome = board.outcome()
+    if outcome.winner is None:
         cprint.cprint.warn('GAME OVER')
-    elif board.outcome().winner == color:
+    elif (outcome.winner == ch.WHITE and color in ['w', 'white']) or (outcome.winner == ch.BLACK and color in ['b', 'black']):
         cprint.cprint.info('YOU WIN')
     else:
         cprint.cprint.fatal('YOU LOSE')
 
-    board.reset()  
+    board.reset()
 
 def get_user_input():
+    """Prompt the user to choose the color and difficulty level for the game."""
     board = ch.Board()
+    save_board_state(board)
 
     color = None
     colors = ['b', 'black', 'w', 'white']
     while color not in colors:
-        color = input('Choose your color (B/W): ').lower()
+        color = input('choose your color (B/W): ').lower()
     color = color.lower()
     
     level = None
@@ -77,11 +95,10 @@ def get_user_input():
         'auto' : 5
     }
     while level not in levels.keys():
-        level = input('Choose difficulty level (auto/easy/medium/difficult): ').lower()
+        level = input('choose difficulty level (auto/easy/medium/difficult): ').lower()
     max_depth = levels[level]
 
-    start_game(board, color, max_depth)      
-
+    start_game(board, color, max_depth)
 
 def main():
     get_user_input()
